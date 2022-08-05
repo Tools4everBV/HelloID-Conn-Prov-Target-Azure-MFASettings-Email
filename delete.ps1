a614ddd29d7fbada63017708755a1c09bf9c8f61
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2022-08-05 15:08:51 +0200

    Updated to support deleted users and improved logging

--- delete.ps1
+++ delete.ps1
@@ -1,10 +1,17 @@
+# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+
+$VerbosePreference = "SilentlyContinue"
+$InformationPreference = "Continue"
+$WarningPreference = "Continue"
+
 #Initialize default properties
-$p = $person | ConvertFrom-Json;
-$m = $manager | ConvertFrom-Json;
-$aRef = $accountReference | ConvertFrom-Json;
-$mRef = $managerAccountReference | ConvertFrom-Json;
-$success = $False;
-$auditLogs = New-Object Collections.Generic.List[PSCustomObject];
+$p = $person | ConvertFrom-Json
+$m = $manager | ConvertFrom-Json
+$aRef = $accountReference | ConvertFrom-Json
+$mRef = $managerAccountReference | ConvertFrom-Json
+$success = $true # Set to true at start, because only when an error occurs it is set to false
+$auditLogs = [Collections.Generic.List[PSCustomObject]]::new()
 
 # AzureAD Application Parameters #
 $config = ConvertFrom-Json $configuration
@@ -13,102 +20,151 @@ $AADtenantID = $config.AADtenantID
 $AADAppId = $config.AADAppId
 $AADAppSecret = $config.AADAppSecret
 
-# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+# Troubleshooting
+# $dryRun = $false
+# $aRef = ''
 
-# Change mapping here
-$account = [PSCustomObject]@{
-    userPrincipalName = $p.Accounts.MicrosoftActiveDirectory.userPrincipalName
-};
-
-$aRef = $account.userPrincipalName
-
-try{
-    Write-Verbose -Verbose "Generating Microsoft Graph API Access Token.."
+# Get current Azure AD user and authentication methods 
+try {
+    Write-Verbose "Generating Microsoft Graph API Access Token"
     $baseUri = "https://login.microsoftonline.com/"
     $authUri = $baseUri + "$AADTenantID/oauth2/token"
 
     $body = @{
-        grant_type      = "client_credentials"
-        client_id       = "$AADAppId"
-        client_secret   = "$AADAppSecret"
-        resource        = "https://graph.microsoft.com"
+        grant_type    = "client_credentials"
+        client_id     = "$AADAppId"
+        client_secret = "$AADAppSecret"
+        resource      = "https://graph.microsoft.com"
     }
 
     $Response = Invoke-RestMethod -Method POST -Uri $authUri -Body $body -ContentType 'application/x-www-form-urlencoded'
-    $accessToken = $Response.access_token;
+    $accessToken = $Response.access_token
 
     #Add the authorization header to the request
     $authorization = @{
-        Authorization = "Bearer $accesstoken";
-        'Content-Type' = "application/json";
-        Accept = "application/json";
+        Authorization  = "Bearer $accesstoken"
+        'Content-Type' = "application/json"
+        Accept         = "application/json"
     }
 
-    Write-Verbose -Verbose "Gathering current Email Authentication Methods for $($account.userPrincipalName).."
+    $baseGraphUri = "https://graph.microsoft.com/"
+    $searchUri = $baseGraphUri + "v1.0/users/$($aRef)"
 
-    $baseUri = "https://graph.microsoft.com/"
-    $getEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($account.userPrincipalName)/authentication/emailMethods"
+    Write-Verbose "Querying Azure AD user with UPN $($aRef)"
+    $azureUser = Invoke-RestMethod -Uri $searchUri -Method Get -Headers $authorization -Verbose:$false
+    if ($null -ne $azureUser.id) { 
+        Write-Verbose "Successfully queried Azure AD user $($azureUser.userPrincipalName) ($($azureUser.id))"
+        # Set aRef to use for further actions
+        $aRef = $azureUser.id
 
-    $getEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $getEmailAuthenticationMethodUri -Method Get -Headers $authorization -Verbose:$false
-    $getEmailAuthenticationMethodResponseValue = $getEmailAuthenticationMethodResponse.value
-    Write-Verbose -Verbose ("Email authentication method: " + ($getEmailAuthenticationMethodResponseValue | Out-String) )
+        Write-Verbose "Gathering current Email Authentication Methods for account with id $($aRef)"
+        $baseUri = "https://graph.microsoft.com/"
+        $getEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($aRef)/authentication/emailMethods"
 
-    $authenticationMethodSet = $false;
-    if( !([string]::IsNullOrEmpty(($getEmailAuthenticationMethodResponseValue | Out-String))) ){
-        $authenticationMethodSet = $true;
+        $getEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $getEmailAuthenticationMethodUri -Method Get -Headers $authorization -Verbose:$fals
+        $getEmailAuthenticationMethodResponseValue = $getEmailAuthenticationMethodResponse.value
+        Write-Verbose ("Current email authentication method: " + ($getEmailAuthenticationMethodResponseValue | Out-String) )
     }
-    
-    if(-Not($dryRun -eq $True)) {
-        if($authenticationMethodSet -eq $false){
-            Write-Verbose -Verbose "No Email Authentication set. Nothing to delete"
-        }else{
-            $currentEmail = ($getEmailAuthenticationMethodResponseValue | Where-Object {$_.id -eq '3ddfcfc8-9383-446f-83cc-3ab9be4be18f'}).emailAddress
-            Write-Verbose -Verbose "Deleting current Email Authentication Method : $currentEmail for $($account.userPrincipalName).."
+}
+catch {
+    $ex = $PSItem
+    $verboseErrorMessage = $ex
+    Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($verboseErrorMessage)"
 
-            $previousAccount = [PSCustomObject]@{
-                userPrincipalName = $account.userPrincipalName
-                email = $currentEmail;
-            }            
+    $auditErrorMessage = ($ex | ConvertFrom-Json).error.message
+    $success = $false  
+    $auditLogs.Add([PSCustomObject]@{
+            Action  = "DeleteAccount"
+            Message = "Error correlating to and updating Azure MFA settings of account with id $($aRef). Error Message: $auditErrorMessage"
+            IsError = $True
+        })
+
+    if ($auditErrorMessage -Like "*Resource '$($aRef)' does not exist*") {
+        if (-Not($dryRun -eq $True)) {
+            $auditLogs.Add([PSCustomObject]@{
+                    Action  = "DeleteAccount"
+                    Message = "No Azure AD user found with UPN $($aRef). Possibly already deleted, skipping action."
+                    IsError = $true
+                })
+        }
+        else {
+            Write-Warning "DryRun: No Azure AD user found with UPN $($aRef). Possibly already deleted, skipping action."
+        }     
+    }
+    else {
+        $success = $false  
+        $auditLogs.Add([PSCustomObject]@{
+                Action  = "DeleteAccount"
+                Message = "Error correlating to and updating Azure MFA settings of account with id $($aRef). Error Message: $auditErrorMessage"
+                IsError = $True
+            })
+    }
+}
+
+if ($null -ne $azureUser.id) {
+    # Remove Email Authentication Method
+    try {
+        # Microsoft docs: https://docs.microsoft.com/nl-nl/graph/api/emailauthenticationmethod-get?view=graph-rest-beta&tabs=http
+        # 3ddfcfc8-9383-446f-83cc-3ab9be4be18f for emailAddress
+        $emailTypeId = '3ddfcfc8-9383-446f-83cc-3ab9be4be18f'
+
+        $authenticationMethodSet = $false
+        if ( !([string]::IsNullOrEmpty(($getEmailAuthenticationMethodResponseValue | Out-String))) ) {
+            $authenticationMethodSet = $true
+        }
+
+        if ($authenticationMethodSet -eq $false) {
+            Write-Verbose "No Email Authentication set for account with id $($aRef). Nothing to delete"
+        }
+        else {
+            $currentEmail = ($getEmailAuthenticationMethodResponseValue | Where-Object { $_.id -eq $emailTypeId }).emailAddress
+            Write-Verbose "Deleting current Email Authentication with value '$($currentEmail)' for account with id $($aRef)"
 
             $baseUri = "https://graph.microsoft.com/"
-            $deleteEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($account.userPrincipalName)/authentication/emailMethods/3ddfcfc8-9383-446f-83cc-3ab9be4be18f"
-        
-            $deleteEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $deleteEmailAuthenticationMethodUri -Method Delete -Headers $authorization -Body $bodyJson -Verbose:$false
-        
-            Write-Verbose -Verbose "Successfully deleted Email Authentication Method : $currentEmail for $($account.userPrincipalName)"
+            $deleteEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($aRef)/authentication/emailMethods/$emailTypeId"
+
+            if (-Not($dryRun -eq $True)) {
+                $deleteEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $deleteEmailAuthenticationMethodUri -Method Delete -Headers $authorization -Body $bodyJson -Verbose:$false
+
+                $auditLogs.Add([PSCustomObject]@{
+                        Action  = "DeleteAccount"
+                        Message = "Successfully deleted Email Authentication Method with value '$($currentEmail)' for account with id $($aRef)"
+                        IsError = $false
+                    })
+            }
+            else {
+                Write-Warning "DryRun: Deleting current Email Authentication Method with value '$($currentEmail)' for account with id $($aRef)"
+            }
         }
     }
+    catch {
+        $ex = $PSItem
+        $verboseErrorMessage = $ex
+        Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($verboseErrorMessage)"
 
-    $auditLogs.Add([PSCustomObject]@{
-        Action = "DeleteAccount"
-        Message = "Updated Azure MFA settings of account with UPN $($aRef)"
-        IsError = $false;
-    });
-
-    $success = $true;    
-}catch{
-    $auditLogs.Add([PSCustomObject]@{
-        Action = "DeleteAccount"
-        Message = "Error updating Azure MFA settings of account with UPN $($aRef): $($_)"
-        IsError = $True
-    });
-    Write-Error $_;
+        $auditErrorMessage = ($ex | ConvertFrom-Json).error.message
+        $success = $false  
+        $auditLogs.Add([PSCustomObject]@{
+                Action  = "DeleteAccount"
+                Message = "Error deleting Email Authentication Method with value '$($currentEmail)' for account with id $($aRef). Error Message: $auditErrorMessage"
+                IsError = $True
+            })
+    }
 }
 
 # Send results
 $result = [PSCustomObject]@{
-	Success= $success;
-	AccountReference= $aRef;
-	AuditLogs = $auditLogs;
-    Account = $account;
-    PreviousAccount = $previousAccount;
+    Success          = $success
+    AccountReference = $aRef
+    AuditLogs        = $auditLogs
+    Account          = $account
+    PreviousAccount  = $previousAccount
 
     # Optionally return data for use in other systems
-    ExportData = [PSCustomObject]@{
+    ExportData       = [PSCustomObject]@{
         userPrincipalName = $account.userPrincipalName
-        email = $account.email;
-    };
-};
+        email             = ''
+    }
+}
 
-Write-Output $result | ConvertTo-Json -Depth 10;
+Write-Output $result | ConvertTo-Json -Depth 10
--- update.ps1
+++ update.ps1
@@ -1,10 +1,17 @@
+# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+
+$VerbosePreference = "SilentlyContinue"
+$InformationPreference = "Continue"
+$WarningPreference = "Continue"
+
 #Initialize default properties
-$p = $person | ConvertFrom-Json;
-$m = $manager | ConvertFrom-Json;
-$aRef = $accountReference | ConvertFrom-Json;
-$mRef = $managerAccountReference | ConvertFrom-Json;
-$success = $False;
-$auditLogs = New-Object Collections.Generic.List[PSCustomObject];
+$p = $person | ConvertFrom-Json
+$m = $manager | ConvertFrom-Json
+$aRef = $accountReference | ConvertFrom-Json
+$mRef = $managerAccountReference | ConvertFrom-Json
+$success = $true # Set to true at start, because only when an error occurs it is set to false
+$auditLogs = [Collections.Generic.List[PSCustomObject]]::new()
 
 # AzureAD Application Parameters #
 $config = ConvertFrom-Json $configuration
@@ -13,125 +20,199 @@ $AADtenantID = $config.AADtenantID
 $AADAppId = $config.AADAppId
 $AADAppSecret = $config.AADAppSecret
 
-# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
-
 # Change mapping here
 $account = [PSCustomObject]@{
-    userPrincipalName       = $p.Accounts.MicrosoftActiveDirectory.userPrincipalName
-    email                   = $p.Contact.Personal.Email;
-    onlySetEmailWhenEmpty   = $false;
-};
+    userPrincipalName     = $p.Accounts.MicrosoftAzureAD.userPrincipalName
+    email                 = $p.Contact.Personal.Email
+    onlySetEmailWhenEmpty = $false
+}
 
-$aRef = $account.userPrincipalName
+# Troubleshooting
+# $dryRun = $false
+# $aRef = ''
+# $account = [PSCustomObject]@{
+#     email                 = 'j.doe@enyoi.nl'
+#     onlySetEmailWhenEmpty = $false
+# }
 
-try{
-    Write-Verbose -Verbose "Generating Microsoft Graph API Access Token.."
+# Get current Azure AD user and authentication methods 
+try {
+    Write-Verbose "Generating Microsoft Graph API Access Token"
     $baseUri = "https://login.microsoftonline.com/"
     $authUri = $baseUri + "$AADTenantID/oauth2/token"
 
     $body = @{
-        grant_type      = "client_credentials"
-        client_id       = "$AADAppId"
-        client_secret   = "$AADAppSecret"
-        resource        = "https://graph.microsoft.com"
+        grant_type    = "client_credentials"
+        client_id     = "$AADAppId"
+        client_secret = "$AADAppSecret"
+        resource      = "https://graph.microsoft.com"
     }
 
     $Response = Invoke-RestMethod -Method POST -Uri $authUri -Body $body -ContentType 'application/x-www-form-urlencoded'
-    $accessToken = $Response.access_token;
+    $accessToken = $Response.access_token
 
     #Add the authorization header to the request
     $authorization = @{
-        Authorization = "Bearer $accesstoken";
-        'Content-Type' = "application/json";
-        Accept = "application/json";
+        Authorization  = "Bearer $accesstoken"
+        'Content-Type' = "application/json"
+        Accept         = "application/json"
     }
 
-    Write-Verbose -Verbose "Gathering current Email Authentication Methods for $($account.userPrincipalName).."
+    $baseGraphUri = "https://graph.microsoft.com/"
+    $searchUri = $baseGraphUri + "v1.0/users/$($account.userPrincipalName)"
 
-    $baseUri = "https://graph.microsoft.com/"
-    $getEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($account.userPrincipalName)/authentication/emailMethods"
+    Write-Verbose "Querying Azure AD user with UPN $($account.userPrincipalName)"
+    $azureUser = Invoke-RestMethod -Uri $searchUri -Method Get -Headers $authorization -Verbose:$false
+    if ($null -ne $azureUser.id) { 
+        Write-Verbose "Successfully queried Azure AD user $($azureUser.userPrincipalName) ($($azureUser.id))"
+        # Set aRef to use for further actions
+        $aRef = $azureUser.id
 
-    $getEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $getEmailAuthenticationMethodUri -Method Get -Headers $authorization -Verbose:$false
-    $getEmailAuthenticationMethodResponseValue = $getEmailAuthenticationMethodResponse.value
-    Write-Verbose -Verbose ("Email authentication method: " + ($getEmailAuthenticationMethodResponseValue | Out-String) )
+        Write-Verbose "Gathering current Email Authentication Methods for account with id $($aRef)"
+        $baseUri = "https://graph.microsoft.com/"
+        $getEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($aRef)/authentication/emailMethods"
 
-    $authenticationMethodSet = $false;
-    if( !([string]::IsNullOrEmpty(($getEmailAuthenticationMethodResponseValue | Out-String))) ){
-        $authenticationMethodSet = $true;
+        $getEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $getEmailAuthenticationMethodUri -Method Get -Headers $authorization -Verbose:$fals
+        $getEmailAuthenticationMethodResponseValue = $getEmailAuthenticationMethodResponse.value
+        Write-Verbose ("Current email authentication method: " + ($getEmailAuthenticationMethodResponseValue | Out-String) )
     }
+}
+catch {
+    $ex = $PSItem
+    $verboseErrorMessage = $ex
+    Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($verboseErrorMessage)"
 
-    if(-Not($dryRun -eq $True)) {    
-        if($authenticationMethodSet -eq $false){
-            Write-Verbose -Verbose "No Email Authentication set. Adding Email Authentication Method : $($account.email) for $($account.userPrincipalName).."
-        
-            $baseUri = "https://graph.microsoft.com/"
-            $addEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($account.userPrincipalName)/authentication/emailMethods"
-        
-            $body = @{
-                "emailAddress" = $($account.email)
+    $auditErrorMessage = ($ex | ConvertFrom-Json).error.message
+    $success = $false  
+    $auditLogs.Add([PSCustomObject]@{
+            Action  = "UpdateAccount"
+            Message = "Error correlating to and updating Azure MFA settings of account with id $($aRef). Error Message: $auditErrorMessage"
+            IsError = $True
+        })
+
+    if ($auditErrorMessage -Like "*Resource '$($account.userPrincipalName)' does not exist*") {
+        if (-Not($dryRun -eq $True)) {
+            $success = $false
+            $auditLogs.Add([PSCustomObject]@{
+                    Action  = "UpdateAccount"
+                    Message = "No Azure AD user found with UPN $($account.userPrincipalName). Possibly deleted."
+                    IsError = $true
+                })
+        }
+        else {
+            Write-Warning "DryRun: No Azure AD user found with UPN $($account.userPrincipalName). Possibly deleted."
+        }
+    }
+    else {
+        $success = $false  
+        $auditLogs.Add([PSCustomObject]@{
+                Action  = "UpdateAccount"
+                Message = "Error correlating to and updating Azure MFA settings of account with id $($aRef). Error Message: $auditErrorMessage"
+                IsError = $True
+            })
+    }
+}
+
+if ($null -ne $azureUser.id) { 
+    # Set Email Authentication Method
+    try {
+        if ( ![string]::IsNullOrEmpty($account.email) ) {
+            # Microsoft docs: https://docs.microsoft.com/nl-nl/graph/api/emailauthenticationmethod-get?view=graph-rest-beta&tabs=http
+            # 3ddfcfc8-9383-446f-83cc-3ab9be4be18f for emailAddress
+            $emailTypeId = '3ddfcfc8-9383-446f-83cc-3ab9be4be18f'
+            $emailAddress = "$($account.email)"
+
+            $authenticationMethodSet = $false
+            if ( !([string]::IsNullOrEmpty(($getEmailAuthenticationMethodResponseValue | Out-String))) ) {
+                $authenticationMethodSet = $true
             }
-            $bodyJson = $body | ConvertTo-Json -Depth 10
-        
-            $addEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $addEmailAuthenticationMethodUri -Method Post -Headers $authorization -Body $bodyJson -Verbose:$false
-        
-            Write-Verbose -Verbose "Successfully updated Email Authentication Method : $($account.email) for $($account.userPrincipalName)"  
-        }else{
-            $currentEmail = ($getEmailAuthenticationMethodResponseValue | Where-Object {$_.id -eq '3ddfcfc8-9383-446f-83cc-3ab9be4be18f'}).emailAddress
-            if($account.onlySetMobileWhenEmpty -eq $true){
-                Write-Verbose -Verbose "Email Authentication Method  set to only update when empty. Since this already contains data ($currentEmail), skipped update for $($account.userPrincipalName)"
-            }else{
-                Write-Verbose -Verbose "Updating current Email Authentication Method : $currentEmail for $($account.userPrincipalName).."
 
-                $previousAccount = [PSCustomObject]@{
-                    userPrincipalName = $account.userPrincipalName
-                    email = $currentEmail;
-                }            
-
+            if ($authenticationMethodSet -eq $false) {
+                Write-Verbose "No Email Authentication set. Adding Email Method with value '$($emailAddress)' for account with id $($aRef)"
+                
                 $baseUri = "https://graph.microsoft.com/"
-                $addEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($account.userPrincipalName)/authentication/emailMethods/3ddfcfc8-9383-446f-83cc-3ab9be4be18f"
-            
+                $addEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($aRef)/authentication/emailMethods"
+
                 $body = @{
-                    "emailAddress" = $($account.email)
+                    "emailAddress" = $($emailAddress)
                 }
                 $bodyJson = $body | ConvertTo-Json -Depth 10
+
+                if (-Not($dryRun -eq $True)) {
+                    $addEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $addEmailAuthenticationMethodUri -Method Post -Headers $authorization -Body $bodyJson -Verbose:$false
+                    
+                    $auditLogs.Add([PSCustomObject]@{
+                            Action  = "UpdateAccount"
+                            Message = "Successfully added Email Authentication Method with value '$($emailAddress)' for account with id $($aRef)"
+                            IsError = $false
+                        })
+                }
+                else {
+                    Write-Warning "DryRun: No Email Authentication set. Adding Email Authentication Method with value '$($emailAddress)' for account with id $($aRef)"
+                }
+            }        
+            else {
+                $currentEmail = ($getEmailAuthenticationMethodResponseValue | Where-Object { $_.id -eq $emailTypeId }).emailAddress
             
-                $addEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $addEmailAuthenticationMethodUri -Method Put -Headers $authorization -Body $bodyJson -Verbose:$false
-            
-                Write-Verbose -Verbose "Successfully updated Email Authentication Method : $($account.email) for $($account.userPrincipalName)"
+                if ($account.onlySetEmailWhenEmpty -eq $true) {
+                    Write-Warning "Email Authentication Method set to only update when empty. Since this already contains data ($currentEmail), skipped update for account with id $($aRef)"
+                }
+                else {
+                    Write-Verbose "Updating current Email Authentication Method value '$currentEmail' to value '$($emailAddress)' for account with id $($aRef)"
+
+                    $baseUri = "https://graph.microsoft.com/"
+                    $addEmailAuthenticationMethodUri = $baseUri + "/beta/users/$($aRef)/authentication/emailMethods/$emailTypeId"
+
+                    $body = @{
+                        "emailAddress" = $($emailAddress)
+                    }
+                    $bodyJson = $body | ConvertTo-Json -Depth 10
+
+                    if (-Not($dryRun -eq $True)) {
+                        $addEmailAuthenticationMethodResponse = Invoke-RestMethod -Uri $addEmailAuthenticationMethodUri -Method Put -Headers $authorization -Body $bodyJson -Verbose:$false
+                
+                        
+                        $auditLogs.Add([PSCustomObject]@{
+                                Action  = "UpdateAccount"
+                                Message = "Successfully updated Email Authentication Method value '$currentEmail' to value '$($emailAddress)' for account with id $($aRef)"
+                                IsError = $false
+                            })
+                    }
+                    else {
+                        Write-Warning "DryRun: Updating current Email Authentication Method value '$currentEmail' to value '$($emailAddress)' for account with id $($aRef)"
+                    }
+                }
             }
         }
     }
+    catch {
+        $ex = $PSItem
+        $verboseErrorMessage = $ex
+        Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($verboseErrorMessage)"
 
-    $auditLogs.Add([PSCustomObject]@{
-        Action = "UpdateAccount"
-        Message = "Updated Azure MFA settings of account with UPN $($aRef)"
-        IsError = $false;
-    });
-
-    $success = $true;    
-}catch{
-    $auditLogs.Add([PSCustomObject]@{
-        Action = "UpdateAccount"
-        Message = "Error cupdating Azure MFA settings of account with UPN $($aRef): $($_)"
-        IsError = $True
-    });
-    Write-Error $_;
+        $auditErrorMessage = ($ex | ConvertFrom-Json).error.message
+        $success = $false  
+        $auditLogs.Add([PSCustomObject]@{
+                Action  = "UpdateAccount"
+                Message = "Error setting Email Authentication Method with value '$($emailAddress)' for account with id $($aRef). Error message: $($auditErrorMessage)"
+                IsError = $True
+            })
+    }
 }
 
 # Send results
 $result = [PSCustomObject]@{
-	Success= $success;
-	AccountReference= $aRef;
-	AuditLogs = $auditLogs;
-    Account = $account;
-    PreviousAccount = $previousAccount;
+    Success          = $success
+    AccountReference = $aRef
+    AuditLogs        = $auditLogs
+    Account          = $account
 
     # Optionally return data for use in other systems
-    ExportData = [PSCustomObject]@{
-        userPrincipalName = $account.userPrincipalName
-        email = $account.email;
-    };
-};
+    ExportData       = [PSCustomObject]@{
+        id                = $azureUser.id
+        userPrincipalName = $azureUser.userPrincipalName
+        email             = $account.email
+    }
+}
 
-Write-Output $result | ConvertTo-Json -Depth 10;
+Write-Output $result | ConvertTo-Json -Depth 10
